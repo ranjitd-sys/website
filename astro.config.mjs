1cb78c75e89e76b3417b13da211101e533bd82f2
ranjitd-sys <ranjitd@deepecom.com>
2026-07-27 12:37:55 +0530

    done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,36 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
+import { defineConfig, fontProviders } from "astro/config"
+import { fileURLToPath } from "node:url"
+import react from "@astrojs/react"
+import tailwindcss from "@tailwindcss/vite"
+const GoogleFontProvider = fontProviders.google()
+export default defineConfig({
+    site: "http://localhost:4321",
+     fonts: [
+    {
+      provider: GoogleFontProvider,
+      name: "Inter",
+      weights: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+      cssVariable: "--font-inter",
+      fallbacks: ["ui-sans-serif", "system-ui", "sans-serif"],
+    },
+    {
+      provider: GoogleFontProvider,
+      name: "JetBrains Mono",
+      weights: ["300", "400", "500", "600", "700"],
+      display: "swap",
+      cssVariable: "--font-jetbrains-mono",
+      fallbacks: ["ui-monospace", "SFMono-Regular", "monospace"],
+    },
+  ],
+  vite: {
+    
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@/": fileURLToPath(new URL("./src/", import.meta.url)),
+      },
+    },
+  },
+  integrations: [react()],
+  // ...fonts, etc.
+})
